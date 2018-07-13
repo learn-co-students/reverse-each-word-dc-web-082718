318eb0f789c4fcae75234a13514ab49c7459896d
Trevor Jameson <tjameson15@gmail.com>
2018-07-13 15:12:09 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,9 @@
 require 'pry'
 
 def reverse_each_word(string)
-  split_arr = string.split(" ")
-  split_arr = split_arr.collect {|word| word.reverse}
-  return split_arr.join(" ")
+  return split_arr = string.split(" ").collect {|word| word.reverse}.join(" ")
 end
 
 
-# Test data:test_str = "Hello there and how are you?"
-# Test call:reverse_each_word(test_str)
+# Test data: test_str = "Hello there and how are you?"
+# Test call: reverse_each_word(test_str)
