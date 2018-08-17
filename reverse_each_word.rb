e8b85f46ef8b4e198b61ed7e5ad08c4197be60f4
Amy Zhou <aymeighz@gmail.com>
2018-08-17 23:05:02 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,10 +7,6 @@ def reverse_each_word(sentence)
 end 
 
 def reverse_each_word(sentence)
-  new_sentence = []
-  sentence.split.collect do |word|
-    new_sentence << word.reverse 
-  end 
-  new_sentence.join(" ")
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end 
 
